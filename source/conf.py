c830ffe9b43ac2c70257cbb03ee068513101831f
Guillaume Bougard <gbougard@teclib.com>
2023-11-20 16:48:25 +0100

    Current GLPI Agent version is now 1.6

--- source/conf.py
+++ source/conf.py
@@ -24,9 +24,9 @@ import sphinx_rtd_theme
 # built documents.
 #
 # The short X.Y version.
-version = u'1.5'
+version = u'1.6'
 # The full version, including alpha/beta/rc tags.
-release = u'1.5'
+release = u'1.6'
 
 project = 'GLPI Agent'
 thisyear = datetime.datetime.now().year
