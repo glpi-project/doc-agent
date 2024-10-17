e82061a0e0520e0279e238da52c2eaa6c7e0f950
Guillaume Bougard <gbougard@teclib.com>
2024-10-17 16:31:49 +0200

    Add tool to publish releases as expected

---
+++ tools/release.sh
@@ -0,0 +1,120 @@
+#! /bin/bash
+
+let FORCE=0 REBASE=0 PUSH=1 NIGHTLY=0
+
+while [ -n "$1" ]
+do
+    case "$1" in
+        --force)
+            let FORCE=1
+            ;;
+        --rebase)
+            let REBASE=1
+            ;;
+        --no-push|--dry-run)
+            let PUSH=0
+            ;;
+        nightly)
+            let NIGHTLY=1
+            ;;
+        *)
+            VERSION="$1"
+            ;;
+    esac
+    shift
+done
+
+set -e
+
+if (( NIGHTLY )); then
+    echo "Updating as nightly..."
+elif [ -z "$VERSION" ]; then
+    echo "No version provided" >&2
+    exit 1
+elif ! echo $VERSION | grep -q -E -e '^[0-9]+\.[0-9]+'; then
+    echo "Not a valid version: $VERSION" >&2
+    exit 1
+fi
+
+if git status -s | grep -q -E '^ ?M+ '; then
+    echo "Some changes are still pending, please commit this changes before using this script" >&2
+    exit 1
+fi
+
+BRANCH=$(git branch --show-current)
+
+# Nightly means to only update master branch with nightly tag without version auto-update
+if (( NIGHTLY )); then
+    if [ "$BRANCH" != "master" ]; then
+        echo "nightly can only be set on master branch" >&2
+        exit 1
+    fi
+    git tag -f nightly
+    if (( PUSH )); then
+        git push
+        git push --tags --force
+    fi
+    exit 0
+fi
+
+# Check if --force option should be used
+if !(( FORCE )); then
+    if git tag -l | grep -q $VERSION; then
+        echo "$VERSION still tagged, use --force to override" >&2
+        exit 1
+    fi
+fi
+
+# Check we are in a dedicated branch
+case "$BRANCH" in
+    "")
+        echo "Not on a branch" >&2
+        exit 1
+        ;;
+    version/$VERSION)
+        # We are still on the expected branch
+        ;;
+    *)
+        if git branch -l version/$VERSION | grep -q version/$VERSION; then
+            # Switch to the branch to rebase if required
+            if (( REBASE )); then
+                git checkout version/$VERSION
+                git rebase $BRANCH
+            else
+                echo "version/$VERSION branch still exists, use --rebase or work from that branch" >&2
+                exit 1
+            fi
+        else
+            # Checkout current branch to new dedicated branch
+            git checkout -b version/$VERSION
+        fi
+        ;;
+esac
+
+# Fix version & release in conf.py
+sed -i -r \
+    -e "s/^version = u'.*'.*/version = u'$VERSION'/" \
+    -e "s/^release = u'.*'.*/release = u'$VERSION'/" \
+    source/conf.py
+
+# Update any |version| found
+grep -lr '|version|' source | grep -v source/conf.py | \
+while read file
+do
+    sed -i -e "s/|version|/$VERSION/g" $file
+done
+
+# Make a commit
+if git status -s | grep -q -E '^ ?M+ '; then
+    git commit -a -m "Update version to $VERSION"
+fi
+
+git tag -f $VERSION
+
+if (( PUSH )); then
+    git push -u origin version/$VERSION
+    git push --tags --force
+fi
+
+# Checkout original branch
+git checkout $BRANCH
