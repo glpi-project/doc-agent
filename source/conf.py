65ebd3efc81136c75365788db4b2c08379f297d8
Guillaume Bougard <gbougard@teclib.com>
2026-04-02 16:53:37 +0200

    Update version in conf

--- source/conf.py
+++ source/conf.py
@@ -24,9 +24,9 @@ import sphinx_rtd_theme
 # built documents.
 #
 # The short X.Y version.
-version = u'1.16'
+version = u'1.17'
 # The full version, including alpha/beta/rc tags.
-release = u'1.16'
+release = u'1.17'
 
 project = 'GLPI Agent'
 thisyear = datetime.datetime.now().year
